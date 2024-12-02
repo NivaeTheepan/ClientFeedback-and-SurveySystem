b6aae864498bfc728e425df66312220db86367d2
Nivaethan Piratheepan <130410644+NivaeTheepan@users.noreply.github.com>
2024-12-02 17:03:16 -0500

    Update script.js

--- backend/feedback/script.js
+++ backend/feedback/script.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose")
 const Feedback = require("./Feedback")
 
-//mongoose.connect("mongodb+srv://npiratheepan:gfYIJ6bLpkysCOrl@cps714-group4.iiqq2.mongodb.net/");
-//mongoose.connect("mongodb://localhost:27017/", {dbName: '714group4'});
 mongoose.connect("mongodb+srv://samih-wadi:ytorZO8J8zYhuGd1@alphabiz.vj5ie.mongodb.net/?retryWrites=true&w=majority&appName=AlphaBiz");
